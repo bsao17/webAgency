1394d214cd2bc0ca404b74a276aaf5276e5cf329
bsao17 <greta.b.meddhed>
2020-04-03 10:20:24 +0200

    debut eet mise en place de la page contact

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-let directionRight = document.getElementById(right);
-let directionLeft = document.getElementById(left);
+var lat = 48.873512268066406;
+var lon = 2.350416421890259;
+var map = null;
+                
+function initMap() {
+                    
+    map = new google.maps.Map(document.getElementById("map"), {                   
+    center: new google.maps.LatLng(lat, lon), 
+    zoom: 19, 
+    mapTypeId: google.maps.MapTypeId.ROADMAP, 
+    mapTypeControl: true,
+    scrollwheel: false, 
+    mapTypeControlOptions: {                        
+        style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR 
+},                    
+navigationControl: true, 
+navigationControlOptions: {                        
+        style: google.maps.NavigationControlStyle.ZOOM_PAN 
+}
+});
+}
+window.onload = function(){                    
+    initMap(); 
+};
